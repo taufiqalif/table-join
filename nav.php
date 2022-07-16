925f9df0a49555ba31855c3b6e47703f6b99a76c
taufiqalif <aliftaufiq4@gmail.com>
2022-07-16 19:12:01 +0700

    UAS

--- nav.php
+++ nav.php
@@ -12,6 +12,9 @@
         <li class="nav-item">
           <a class="nav-link" href="right.php">Right</a>
         </li>
+        <li class="nav-item">
+          <a class="nav-link" href="uas.php">UAS</a>
+        </li>
       </ul>
     </div>
   </div>
